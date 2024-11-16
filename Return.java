ba1830936ced802175bd8d30bf0fd440f0026564
Bat_Gosho <goshkodark25@gmail.com>
2024-11-16 21:20:02 +0200

    Add files via upload

---
+++ Return.java
@@ -0,0 +1,9 @@
+public class Return {
+    public static int cube(int n1){
+        return n1*n1*n1;
+    }
+    public static void main(String[] args){
+        int power = cube(5);
+        System.out.println(power);
+    }
+}
