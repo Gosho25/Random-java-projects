3b6a8329b236590f4efd3116e09730361a29b522
Bat_Gosho <goshkodark25@gmail.com>
2024-11-17 11:52:36 +0200

    Add files via upload

---
+++ NewWay.java
@@ -0,0 +1,7 @@
+import java.lang.String;
+public class NewWay {
+    public static void main(String[] args) {
+        String car = "Volvo";
+        System.out.println(car.length());
+    }
+}
---
+++ String.java
@@ -0,0 +1,8 @@
+public class String {
+    public static java.lang.String createFullName(String firstName, String lastName) {
+        return firstName + "" + lastName;
+    }
+
+}
+
+
