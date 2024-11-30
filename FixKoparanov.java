9c57bc38adb850e1ea8905ce514d8d24becac441
Bat_Gosho <goshkodark25@gmail.com>
2024-11-30 16:22:31 +0200

    Add files via upload

---
+++ FixKoparanov.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class FixKoparanov {
+    public static void main(String[] args) {
+        String[] list = new String[31];
+        list[0] = "Александрър";
+        list[1] = "Александър2";
+        list[2] = "Борис";
+        list[3] = "==========";
+        list[4] = "Георги";
+        list[5] = "Давид";
+        list[6] = "";
+        list[7] = "";
+        list[8] = "";
+        list[9] = "";
+        list[10] = "";
+        list[11] = "";
+        list[12] = "";
+        list[13] = "";
+        list[14] = "";
+        list[15] = "";
+        list[16] = "";
+        list[17] = "";
+        list[18] = "";
+        list[19] = "";
+        list[20] = "";
+        list[21] = "";
+        list[22] = "";
+        list[23] = "";
+        list[24] = "";
+        list[25] = "";
+        list[26] = "";
+        list[27] = "";
+        list[28] = "";
+        list[29] = "Алекзандър";
+
+        /*
+        Добави имената
+        провери интерациите list.leght - 1
+        напиши отсиствията
+         */
+    }
+     /*
+    public static int[] removeElement(int[] originalArray, int indexToRemove) {
+        if (indexToRemove < 0 || indexToRemove >= originalArray.length) {
+            return originalArray;  // Ако индексът е невалиден, връщаме оригиналния масив
+        }
+
+        // Създаваме нов масив с размер -1
+        int[] newArray = new int[originalArray.length - 1];
+
+        // Копираме елементите от оригиналния масив в новия масив, пропускайки елемента на дадения индекс
+        for (int i = 0, j = 0; i < originalArray.length; i++) {
+            if (i == indexToRemove) {
+                continue; // Пропускаме елемента на индекс 'indexToRemove'
+            }
+            newArray[j++] = originalArray[i];
+        }
+
+        return newArray;
+    }
+    */
+
+}
