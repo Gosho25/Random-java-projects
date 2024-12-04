c2b2f739f9b1d6a879a71c39575ac262eef226ed
Bat_Gosho <goshkodark25@gmail.com>
2024-12-04 16:33:54 +0200

    Add files via upload

---
+++ len.java
@@ -0,0 +1,8 @@
+import java.lang.reflect.Array;
+
+public class len {
+    public static void main(String[] args) {
+        String daTeEba = "chukurChukur";
+        System.out.println(daTeEba.length());
+    }
+}
