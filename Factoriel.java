4cf312490cdc753318bdbcf80eb07e0db026e10f
Bat_Gosho <goshkodark25@gmail.com>
2024-12-06 21:28:12 +0200

    Add files via upload

---
+++ Factoriel.java
@@ -0,0 +1,9 @@
+public class Factoriel {
+    public static int fact(int num) {
+        int result = 1;
+        for(int i = 1; i <= num; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
