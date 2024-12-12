220c88d1eb9be157d708e53780293fffea11c1dc
Bat_Gosho <goshkodark25@gmail.com>
2024-12-12 16:50:26 +0200

    Update Call.java

--- Call.java
+++ Call.java
@@ -2,7 +2,7 @@ public class Call {
     public static void main(String[] args) {
         hello();
         hello();
-        hello();
+        hello(); 
     }
     static void hello() {
         System.out.println("Hello World!");
