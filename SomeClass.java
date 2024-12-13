5c9fd63616fbf568998550b134d17efa39d61aed
Bat_Gosho <goshkodark25@gmail.com>
2024-12-13 23:52:09 +0200

    Add files via upload

---
+++ SomeClass.java
@@ -0,0 +1,12 @@
+class SomeClass {
+    String foo = "something";
+    String bar = "cool";
+}
+
+class Main {
+    public static void main(String[] args) {
+        SomeClass obj = new SomeClass();
+        System.out.println(obj.foo);
+        System.out.println(obj.bar);
+    }
+}
