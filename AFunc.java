9104763ed4a620309d15876d9600a7456593b2ae
Bat_Gosho <goshkodark25@gmail.com>
2024-12-14 21:17:51 +0200

    Update AFunc.java

--- AFunc.java
+++ AFunc.java
@@ -1,6 +1,6 @@
 public class AFunc {
     public  static void printSome(String[] arr){
         System.out.println(arr[0]);
-        System.out.println(arr[2]);
+        System.out.println(arr[2]); 
     }
 }
