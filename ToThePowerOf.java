cbb8c3bba0f4ad6a7c4b41d3a945e6c11328125b
Bat_Gosho <goshkodark25@gmail.com>
2024-12-14 15:36:33 +0200

    Add files via upload

---
+++ ToThePowerOf.java
@@ -0,0 +1,10 @@
+
+
+public class ToThePowerOf {
+    public static void main(String[] args) {
+        int n1 = 2;
+        int pow = 64646464;
+        int result = (int)Math.pow(n1, pow);
+        System.out.println(result);
+    }
+}
