fed7482e4e2ddf4582e8becd0f3e4155d19c1272
Bat_Gosho <goshkodark25@gmail.com>
2024-12-20 09:59:11 +0200

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,20 @@
+public class Arithmetic {
+    public int num1;//1
+    public int num2;//2
+
+    public Arithmetic(int num1, int num2){// 2 i 2
+        this.num1 = num1;//this 1 = 2
+        this.num2 = num2;
+    }
+    public void sum(){
+        System.out.println(num1 + num2);
+    }
+    
+}
+
+class Result{
+    public static void main(String[] args) {
+        Arithmetic result = new Arithmetic(2, 2);
+        result.sum();
+    }
+}
