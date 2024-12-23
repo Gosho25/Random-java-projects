0667b9f794fd1fb2c2b7a2e834802e2557b6ede9
Bat_Gosho <goshkodark25@gmail.com>
2024-12-23 23:41:49 +0200

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,7 +1,7 @@
 public class Array {
     public static void main(String[] args) {
         double[] coolNums = {3.14, 1.68, 9.99};
-        System.out.println(coolNums.length);
+        System.out.println(coolNums.length); 
 
         System.out.print(coolNums[0]); 
         System.out.print(", "); 
