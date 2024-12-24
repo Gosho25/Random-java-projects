2ecd6bab8d28ee98994550271983aa7e7cf621f3
Bat_Gosho <goshkodark25@gmail.com>
2024-12-24 21:59:47 +0200

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,10 @@
+public class Discount {
+    public static float discount(float price, float disc){
+        return price * disc;
+    }
+
+    public static void main(String[] args) {
+        float printing = discount(1500, 0.5f);
+        System.out.println((int)printing);
+    }
+}
