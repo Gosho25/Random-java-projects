bc1c5dd7b98eb0c09873257f5b93156bb5b22972
Bat_Gosho <goshkodark25@gmail.com>
2024-12-25 23:28:09 +0200

    Add files via upload

---
+++ AgeTill120.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AgeTill120 {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = scan.nextInt();
+
+        int till120 = 100 - age;
+        System.out.println(till120);
+    }
+}
