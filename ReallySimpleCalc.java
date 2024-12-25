9cedee5d81bf739f686710b9cd69726c9ad243fa
Bat_Gosho <goshkodark25@gmail.com>
2024-12-25 23:26:31 +0200

    Add files via upload

---
+++ ReallySimpleCalc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReallySimpleCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char op = sc.next().charAt(0);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int res;
+        if(op == '+'){
+            res = n1 + n2;
+        } else if(op == '-') {
+            res = n1 - n2;
+        }else if(op == '*'){
+            res = n1 * n2;
+        }else if(op == '/'){
+            res = n1 / n2;
+        }else{
+            res = 0;
+        }
+        System.out.println(res);
+    }
+}
