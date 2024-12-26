16cf588d31cdbc9fdad0bf6158b37a6e630f379a
Bat_Gosho <goshkodark25@gmail.com>
2024-12-26 22:25:12 +0200

    Add files via upload

---
+++ Valko.java
@@ -0,0 +1,28 @@
+public class Valko {
+    public int age;
+    public String name;
+    public  int height;
+
+     public Valko(int age, String name, int height){
+         this.age = age;
+         this.name = name;
+         this.height = height;
+     }
+
+     public void prtInf(){
+         System.out.println(name);
+         System.out.println(age);
+         System.out.println(height);
+     }
+}
+/*
+class PrintInfo{
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+basic class
+ */
