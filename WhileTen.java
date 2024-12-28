75d7740a7173badf6d851f9208daa5637d98001e
Bat_Gosho <goshkodark25@gmail.com>
2024-12-28 17:27:25 +0200

    Add files via upload

---
+++ WhileTen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WhileTen {
+    public static void main(String[] args) {
+        /*
+        int count = 0;
+        while(true){
+            System.out.printf("hello\n");
+            count++;
+            if(count==10){
+                break;
+            }
+        }
+         */
+
+        int i = 0;
+        while(i <  10){
+            i++;
+            System.out.printf(i +".hello\n");
+        }
+    }
+}
