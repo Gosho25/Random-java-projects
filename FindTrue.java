bd0daa5bc6d024ea566a555e24f9142c7a451236
Bat_Gosho <goshkodark25@gmail.com>
2025-01-07 21:34:05 +0200

    Update FindTrue.java

--- FindTrue.java
+++ FindTrue.java
@@ -2,8 +2,22 @@ import java.util.ArrayList;
 
 public class FindTrue {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         ArrayList<Boolean> list = new ArrayList<>();
         //add USER INPUT boolean stoinosti
+        /*
+        System.out.println("Enter elements: ");
+        int elements = sc.nextInt();
+        int counter = 0;
+        while(true){
+            list.add(sc.hasNextByte());
+            counter++;
+            if(counter > elements){
+                break;
+            }
+        }
+         */
         
     }
 }
