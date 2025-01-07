2eaa24ac81557826662c2fe4a23c4e85d9ea3b90
Bat_Gosho <goshkodark25@gmail.com>
2025-01-07 15:34:58 +0200

    Update FindTrue.java

--- FindTrue.java
+++ FindTrue.java
@@ -1,6 +1,4 @@
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class FindTrue {
     public static void main(String[] args) {
@@ -9,17 +7,3 @@ public class FindTrue {
         
     }
 }
-
-
-
-       /*
-       Create a function which returns the number of true values there are in an array.
-
-Examples
-countTrue([true, false, false, true, false]) ➞ 2
-
-countTrue([false, false, false, false]) ➞ 0
-
-countTrue([]) ➞ 0
-
-        */
