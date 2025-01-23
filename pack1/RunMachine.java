219f5fabab3f3a29e11a60cb7f4c7cd596584dbb
Bat_Gosho <goshkodark25@gmail.com>
2025-01-23 21:58:40 +0200

    Add files via upload

---
+++ pack1/RunMachine.java
@@ -0,0 +1,9 @@
+package pack1;
+
+import java.util.Random;
+
+public class RunMachine {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ pack1/Sorse.java
@@ -0,0 +1,6 @@
+package pack1;
+
+public class Sorse {
+    //first class-called with methods(or the only one it's the beginning)
+
+}
