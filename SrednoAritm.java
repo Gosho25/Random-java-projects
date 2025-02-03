74ec8784280d4444feaa43525bba693bb8326c9d
Bat_Gosho <goshkodark25@gmail.com>
2025-02-03 21:50:42 +0200

    Add files via upload

---
+++ SrednoAritm.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SrednoAritm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 1;i <= 5;i++){
+            int userNum = sc.nextInt();
+            sum += userNum;
+        }
+        int result = sum / 5;
+        System.out.println(result);
+    }
+}
