d8d4bddc0c60beb15df3c15510528c03c67fea3f
Bat_Gosho <goshkodark25@gmail.com>
2025-02-04 12:43:15 +0200

    Add files via upload

---
+++ TernalOperator.java
@@ -0,0 +1,8 @@
+public class TernalOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = a > b ? 1 : 0;//1 is expr1 and 0 is expr2, a > b is condition
+        System.out.println(result);//I use 1 and 0 like in C
+    }
+}
