961dda198099ffbbb35cd2bfe7729e18b76fb1be
Bat_Gosho <goshkodark25@gmail.com>
2025-02-04 12:23:04 +0200

    Update SrednoAritm.java

--- SrednoAritm.java
+++ SrednoAritm.java
@@ -11,5 +11,17 @@ public class SrednoAritm {
         int result = sum / 5; 
         System.out.println(result);
         //upgrade from 5 to N(user's numbers)
+        /*
+        System.out.println("Write how many numbers you want to write: ");
+        int lenght = sc.nextInt();
+        int sum = 0;
+        System.out.println();
+        for(int i = 1;i <= lenght;i++){
+            int userNum = sc.nextInt();
+            sum += userNum;
+        }
+        int result = sum / lenght;
+        System.out.println(result);
+        */
     }
 }
