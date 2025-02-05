ae1e77d57e87ceea4523d9e862d23a67a29be27a
Bat_Gosho <goshkodark25@gmail.com>
2025-02-05 22:36:27 +0200

    Add files via upload

---
+++ SQRT.java
@@ -0,0 +1,9 @@
+public class SQRT {
+    public static void main(String[] args) {
+        int a = 6;
+        double res = Math.pow(a, 0.5);
+        double res2 = Math.sqrt(a);
+        System.out.println(res);
+        System.out.println(res2);
+    }
+}
