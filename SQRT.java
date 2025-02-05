aab06e3d73156a389529cd08896389c69a43deaa
Bat_Gosho <goshkodark25@gmail.com>
2025-02-05 22:38:07 +0200

    Update SQRT.java

--- SQRT.java
+++ SQRT.java
@@ -1,7 +1,7 @@
 public class SQRT {
     public static void main(String[] args) {
         int a = 6;
-        double res = Math.pow(a, 0.5);//1 variant
+        double res = Math.pow(a, 0.5);//1 variant(sqrt of X number(like 6) is X to the power of 1/2 or 0.5)
         double res2 = Math.sqrt(a);//2 variant(better and shorter)
         System.out.println(res);
         System.out.println(res2);
