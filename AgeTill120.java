832ff3f39447e9797744f476cc99b17c570b5754
Bat_Gosho <goshkodark25@gmail.com>
2025-02-10 17:00:39 +0200

    Update AgeTill120.java

--- AgeTill120.java
+++ AgeTill120.java
@@ -4,7 +4,7 @@ public class AgeTill120 {
     public static void main(String[] args) {
         Scanner scan  = new Scanner(System.in);
 
-        System.out.println("Enter your age: ");
+        System.out.println("Enter your age: "); 
         int age = scan.nextInt();  
 
         int till120 = 100 - age;  
