9fe939d877e20e9019122ebfed69627c13c92c97
Bat_Gosho <goshkodark25@gmail.com>
2025-02-24 22:35:58 +0200

    Add files via upload

--- Factoriel.java
+++ Factoriel.java
@@ -1,9 +1,24 @@
 public class Factoriel {
-    public static int fact(int num) {
-        int result = 1;
-        for(int i = 1; i <= num; i++){
-            result *= i;
+    int num;
+
+    public Factoriel(int num){
+        this.num = num;
+    }
+    public void fact() {
+        int result = 0;
+        for(int i = 1; i <= num*2; i++){
+            result += i;
         }
-        return result;
+        System.out.println(result);
+    }
+}
+
+
+
+class Pepi{
+    public static void main(String[] args) {
+        Factoriel factor = new Factoriel(3);
+        factor.fact();
+
     }
 }
