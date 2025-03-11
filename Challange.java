3aa6d1af51896fe207a4942feef16f1ef649f503
Bat_Gosho <goshkodark25@gmail.com>
2025-03-11 22:14:54 +0200

    Update Challange.java

--- Challange.java
+++ Challange.java
@@ -1,6 +1,6 @@
 public class Challange {
     public static String getFirstPart(String s) {
-        if(s.contains("," )){
+        if(s.contains("," )){ 
             int commaIndex = s.indexOf(",");
             return s.substring(0, commaIndex);
         }else{  
